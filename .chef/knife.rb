414680e7d35d18494e37d5452244e789266cdd25
Ubuntu <ubuntu@ip-172-31-18-217.ap-southeast-1.compute.internal>
2015-01-07 02:27:05 +0000

    added aws access key in .chef/knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,1 +1,5 @@
-cookbook_path [ '/home/alex/chef-repo/cookbooks' ]
+cookbook_path [ '/home/ubuntu/chef-repo/cookbooks' ]
+
+knife[:aws_access_key_id] = "AKIAJXLSW2DWKUFTOT7Q"
+knife[:aws_secret_access_key] = "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"
+knife[:aws_ssh_key_id] = "uohzxela2-key-pair-ap-southeast1"
